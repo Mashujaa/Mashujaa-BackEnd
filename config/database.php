83993414b7083c08635fc704a95d676eef318956
mbuthi <mbuthimungai34@gmail.com>
2022-11-15 09:10:16 +0300

    Adding database configuration file

--- config/database.php
+++ config/database.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Str;
+$host = env('DB_HOST', '127.0.0.1');
+$database = env('DB_DATABASE', '');
+$username = env('DB_USERNAME', 'forge');
+$password = env('DB_PASSWORD', 'forge');
 
+
+if($databaseUrl = getenv('DATABASE_URL')) {
+
+    $url = parse_url($databaseUrl);
+
+    $host = $url['host'];
+    $username = $url['user'];
+    $password = $url['pass'];
+    $database = substr($url['path'], 1);
+}
 return [
 
     /*
@@ -68,15 +82,28 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'prefer',
         ],
+        // 'pgsql' => [
+        //     'driver' => 'pgsql',
+        //     'host' => $host,
+        //     'port' => env('DB_PORT', '5432'),
+        //     'database' => $database,
+        //     'username' => $username,
+        //     'password' => $password,
+        //     'charset' => 'utf8',
+        //     'prefix' => '',
+        //     'schema' => 'public',
+        //     'sslmode' => 'prefer',
+        // ],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
