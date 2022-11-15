be967c0815ee4770b342daa6adab08fad53f94f7
mbuthi <mbuthimungai34@gmail.com>
2022-11-15 08:50:42 +0300

    changing logging file

--- config/logging.php
+++ config/logging.php
@@ -58,8 +58,8 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'driver' => 'errorlog',
+            // 'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
