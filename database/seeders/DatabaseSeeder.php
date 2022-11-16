84ac710aa4111717a2ca109a232193e1dc4488e6
mbuthi <mbuthimungai34@gmail.com>
2022-11-16 15:12:09 +0300

    Add New Tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Faker\Factory;
 class DatabaseSeeder extends Seeder
 {
     /**
