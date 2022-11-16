936591fca2c538126e9a910c9fe639895a757b7c
mbuthi <mbuthimungai34@gmail.com>
2022-11-16 06:50:48 +0300

    Change student controller

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id("user_id");
             $table->string('unique_identifier')->unique();
             $table->string('password')->default('');
             $table->timestamp('email_verified_at')->nullable();            
